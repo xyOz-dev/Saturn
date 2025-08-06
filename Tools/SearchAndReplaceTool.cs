206b248639d4641d51b8c6159116f0823e8338a3
xyOz <xyoz-dev@proton.me>
2025-08-06 12:31:21 +0100

    Add missing FileSystemGlobbing.Abstractions import
    Added the Microsoft.Extensions.FileSystemGlobbing.Abstractions namespace import to SearchAndReplaceTool.cs to ensure access to required abstractions for file system globbing functionality.

--- Tools/SearchAndReplaceTool.cs
+++ Tools/SearchAndReplaceTool.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 using Saturn.Tools.Core;
 
 namespace Saturn.Tools
